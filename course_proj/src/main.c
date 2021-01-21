fbd3980a1fb435f2599b0c887468275e407df4a3
Doshchinsky <akter1997@gmail.com>
2021-01-21 16:54:31 +0700

    Adapted for Sergo

--- course_proj/src/main.c
+++ course_proj/src/main.c
@@ -20,9 +20,18 @@ int main(int argc, char **argv) {
   if (rank == src) {
     strcpy(message, argv[1]);
   }
-  rmpi_bcast(message, 10, RMPI_TYPE_CHAR, src, 0, RMPI_COMM_WORLD);
+//rmpi_bcast(message, 10, RMPI_TYPE_CHAR, src, 0, RMPI_COMM_WORLD);
 
-  sprintf(log_buffer, "Rank %d/%d [Bcast rank %s]", rank, commsize, message);
+//sprintf(log_buffer, "Rank %d/%d [Bcast rank %s]", rank, commsize, message);
+//logInfo(log_buffer);
+  if (rank == 1) {
+    sprintf(message, "%d", rank);
+    rmpi_send(message, 10, RMPI_TYPE_CHAR, 0, 0, RMPI_COMM_WORLD);
+  }
+  if (rank == 0) {
+    rmpi_recv(message, 10, RMPI_TYPE_CHAR, 1, 0, RMPI_COMM_WORLD);
+  }
+  sprintf(log_buffer, "Rank %d/%d [Diff rank %s]", rank, commsize, message);
   logInfo(log_buffer);
 
   logSys("Sleep");
